629e686b18d5e936b3afc79f13696ddf65c2fa25
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-01-24 10:20:55 -0800

    Alx go Stress U

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+import { useContext } from "react";
+import UserContext from "../UserContext"; // adjust path if needed
 
-const UserProfile = (props) => {
-    return(
-        <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-        
-        </div>
-    );
-};
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
 
 export default UserProfile;
