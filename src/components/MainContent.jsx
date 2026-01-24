03b76c9db72b7f30a7027ef9af1c26f7189a09f1
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-01-24 07:23:38 -0800

    gitWahala

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,24 @@
+function MainContent () {
+    return (
+        <div> 
+            <span
+
+                    style={{
+                    color: 'blue',
+                    border: '1px solid blue',
+                    padding: '10px',
+                    margin: '10px',
+                    }}
+            >
+    
+                </span>
+            <main>
+
+               <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+
+        </div>
+    )
+}
+
+export default MainContent;
