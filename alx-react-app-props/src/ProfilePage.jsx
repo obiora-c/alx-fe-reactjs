7a078678deedbd1a0511d453a7e8352c7f2a9633
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-01-24 09:41:43 -0800

    MyG

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,10 @@
+
+
+// src/ProfilePage.jsx
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+function Footer () {
+    return (
+        <div>
+            <footer>
+              <p>© 2023 City Lovers</p>
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer;
