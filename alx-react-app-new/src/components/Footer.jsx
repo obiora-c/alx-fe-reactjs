f2014bb9aab9d7fdd5ab244635e99d050f0e5fcc
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-01-24 03:40:07 -0800

    New

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+
+
+function Footer () {
+    return (
+        <div>
+            <footer style={{
+                backgroundColor: "#222",
+                color: "white",
+                textAlign: "center",
+                padding: "10px",
+                marginTop: "20px",
+            }}>
+              <p>© 2023 City Lovers</p>
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent () {
+    return (
+        <div> 
+            <main  style={{
+            backgroundColor : 'pink',
+            textAlign : 'center',
+            padding : '10px',
+        }}>
+               <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+
+        </div>
+    )
+}
+
+export default MainContent;
