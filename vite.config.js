c96d821eca82317eca3140a7cb799a612215ef14
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-01-31 03:53:39 -0800

    API

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
 
   server: {
     port: 5174,
     strictPort: true
-  },
+  }
 })
