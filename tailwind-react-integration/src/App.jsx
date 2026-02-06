3c6c19b45f9a8dc323d92582c129f93935c79b52
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-02-06 06:57:59 -0800

    TailwindCss

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <UserProfile />
+  
+ 
+    </>
+  )
+}
+
+export default App;
