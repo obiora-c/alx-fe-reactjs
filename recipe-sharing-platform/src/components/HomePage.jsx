a5c906cbfa21b8219aa24959cbdc3771aefa55ff
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-02-12 02:46:05 -0800

    Recipe-P

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
       </h1>
 
       {/* Recipes Grid */}
-      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
