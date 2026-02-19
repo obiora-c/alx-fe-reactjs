2175d5ff7dd01af95d00df4f6df919c4ca883de2
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-02-19 05:27:00 -0800

    query01

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,18 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
-<QueryClientProvider client={queryClient}>
-  <App />
-</QueryClientProvider>
+  </React.StrictMode>
+);
