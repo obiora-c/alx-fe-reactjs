0766c1d154cec9c12e3bef25cd8ef72e926e39ab
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-02-19 06:50:13 -0800

    Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom"; // <-- only Routes and Route here
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
